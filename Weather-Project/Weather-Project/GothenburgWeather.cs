c59cc813b00bfa5c3d85524a892630edf93b6f3e
Wael Hasan <wael850224@gmail.com>
2020-01-29 22:44:30 +0100

    Clear

---
+++ Weather-Project/Weather-Project/GothenburgWeather.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherProject
+{
+
+    public class GothenburgWeather
+    {
+        public async static Task<RootObject> GetWeather()
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("https://api.openweathermap.org/data/2.5/weather?lat=57.709203&lon=11.9642338&appid=2fd1aaaa85e1d7d52c0b96d4920346a4&units=metric&lang=se");
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new System.IO.MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public Main main { get; set; }
+        [DataMember]
+        public Wind wind { get; set; }
+        [DataMember]
+        //public Clouds clouds { get; set; }
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+    }
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+        [DataMember]
+        public double feels_like { get; set; }
+        [DataMember]
+        public double temp_min { get; set; }
+        [DataMember]
+        public double temp_max { get; set; }
+    }
+
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Weather-Project/Weather-Project/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using WeatherProject;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Weather_Project
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView appView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            appView.Title = "Gothenburg Weather";
+            DegreeGetter();
+            FeelsLike();
+            CityName();
+            MaxCelsius();
+            MinCelsius();
+            ShowWind();
+            GetDescription();
+        }
+
+        private async void DegreeGetter()
+        {
+            RootObject gotWeather =
+                await GothenburgWeather.GetWeather();
+
+            string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", gotWeather.weather[0].icon);
+            ResultImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(icon, UriKind.Absolute));
+            celsius.Text = ((int)gotWeather.main.temp).ToString() + "°";
+        }
+        private async void GetDescription()
+        {
+            RootObject gotWeather = await GothenburgWeather.GetWeather();
+            textDescription.Text = gotWeather.weather[0].description[0].ToString().ToUpper()
+          + gotWeather.weather[0].description.Substring(1);
+        }
+        private async void CityName()
+        {
+            RootObject gotWeather = await GothenburgWeather.GetWeather();
+            cityResult.Text = gotWeather.name;
+        }
+        private async void MaxCelsius()
+        {
+            RootObject gotWeather = await GothenburgWeather.GetWeather();
+            Max_Celsius.Text = "Max: " + ((int)gotWeather.main.temp_max).ToString() + "°";
+        }
+        private async void MinCelsius()
+        {
+            RootObject gotWeather = await GothenburgWeather.GetWeather();
+            Min_Celsius.Text = "Min: " + ((int)gotWeather.main.temp_min).ToString() + "°";
+        }
+        private async void ShowWind()
+        {
+            RootObject gotWeather = await GothenburgWeather.GetWeather();
+            WindSpeedText.Text = "Vind: " + ((int)gotWeather.wind.speed).ToString() + " m/s";
+        }
+        private async void FeelsLike()
+        {
+            RootObject gotWeather =
+                await GothenburgWeather.GetWeather();
+            TextFeelsLike.Text = "Känns: " + ((int)gotWeather.main.feels_like).ToString() + "°";
+        }
+    }
+
+
+}
+
+
